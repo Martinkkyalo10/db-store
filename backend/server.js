e35818e1a8e8151a68a6d006c27645bc3a73186c
Martin Kyalo <martinkkyalo10@gmail.com>
2022-07-01 21:54:26 +0300

    cors removed

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import http from 'http';
 import { Server } from 'socket.io';
 import express from 'express';
 import mongoose from 'mongoose';
+// import cors from 'cors';
 import dotenv from 'dotenv';
 import path from 'path';
 import productRouter from './routers/productRouter.js';
@@ -12,6 +13,7 @@ import uploadRouter from './routers/uploadRouter.js';
 dotenv.config();
 
 const app = express();
+// app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
